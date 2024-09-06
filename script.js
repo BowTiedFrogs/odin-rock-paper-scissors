0ca90d27addc1a32678709663a89443477c46fa3
BowTiedFrogs <bowtiedfrogs@protonmail.com>
2024-09-06 12:14:09 +0300

    add first version of solution
    Changed files:
    * index.html
    * script.js
    * style.css

---
+++ script.js
@@ -0,0 +1,53 @@
+function getComputerChoice() {
+    const computerChoice = ['rock', 'paper', 'scissors'];
+    const randomIndex = Math.floor(Math.random() * computerChoice.length);
+    return computerChoice[randomIndex];
+}
+
+function getHumanChoice() {
+    const availableChoices = ['rock', 'paper', 'scissors'];
+    
+    while (true) {
+        let userChoice = prompt("Enter your choice (rock/paper/scissors):").toLowerCase();
+        
+        if (availableChoices.includes(userChoice)) {
+            return userChoice;
+        } else {
+            console.log("Invalid input, try again");
+        }
+    }
+}
+
+let computerScore = 0
+let humanScore = 0
+
+function playRound(humanChoice, computerChoice) {
+    console.log(`You chose: ${humanChoice}`);
+    console.log(`Computer chose: ${computerChoice}`);
+
+    if (humanChoice === computerChoice) {
+        return console.log("It's a tie!");
+    }
+
+    if (
+        (humanChoice === 'paper' && computerChoice === 'rock') ||
+        (humanChoice === 'rock' && computerChoice === 'scissors') ||
+        (humanChoice === 'scissors' && computerChoice === 'paper')
+    )
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
+function playGame() {
+
+    {
+        humanScore++;
+        return console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+    } else {
+        computerScore++;
+        return console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+    }
+}
